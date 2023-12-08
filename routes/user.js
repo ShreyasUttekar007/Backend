35559cd9be825ac843bdea37fbceb43aa76c9fbc
ShreyasUttekar007 <shreyas.uttekar@showtimeconsulting.in>
2023-12-08 12:23:12 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs").promises;
+const path = require("path");
+const FormModel = require("../models/FormModel");
+
+const router = express.Router();
+
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
+
+router.post("/submit", upload.single("image"), async (req, res) => {
+  try {
+    const formData = req.body;
+
+    const formEntry = new FormModel({
+      code:formData.code,
+      name: formData.name,
+      phoneNumber: formData.phoneNumber,
+      dob: formData.dob,
+      gender: formData.gender,
+      district: formData.district,
+      university: formData.university,
+      pincode: formData.pincode,
+      image: req.file.buffer.toString("base64"), 
+    });
+
+    await formEntry.save();
+
+    res
+      .status(201)
+      .json({ message: "Form submitted successfully", code: formEntry.code });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/all", async (req, res) => {
+  try {
+    const allFormData = await FormModel.find({}, { code: 1, name: 1, phoneNumber: 1, dob: 1, gender: 1, district: 1, university: 1, pincode: 1, _id: 0, image: 1 });
+    res.status(200).json(allFormData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/user');
+const config = require("./config");
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }));
+
+console.log(process.env.MONGODB_URI, 'aefafaf');
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+app.use('/api', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server connected to port ${PORT}`);
+});
