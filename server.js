27d371bbd33b5a1fb632e4b28145f43aec699bdb
ShreyasUttekar007 <shreyas.uttekar@showtimeconsulting.in>
2023-12-08 16:24:11 +0530

    Db change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/user');
 const config = require("./config");
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
